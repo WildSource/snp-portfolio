7481964b0edd0b00cbf03944cc025cbd239e85d5
WildSource <ilovetrap123@hotmail.com>
2025-01-23 20:14:53 -0500

    feat: add fonts

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,9 +3,12 @@ import './About.css'
 function About() {
   return (
     <>
-      <img></img>
-      <h2>About Me</h2>
-      <p></p>
+      <h2><strong>About Me</strong></h2>
+      <p>
+        Hi, my name is Samuel Nguyen-Phok. <br></br> 
+        I am a web developer based in Quebec, Canada. <br></br> 
+        Although I can do frontend work, I prefer working on the backend.
+      </p>
     </>
   );
 }
