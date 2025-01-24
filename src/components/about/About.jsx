e5efb7511de7cffc3a4105b4ea80bdb9f0396964
WildSource <ilovetrap123@hotmail.com>
2025-01-24 06:26:26 -0500

    feat: about me content implemented

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,9 +5,27 @@ function About() {
     <>
       <h2><strong>About Me</strong></h2>
       <p>
-        Hi, my name is Samuel Nguyen-Phok. <br></br> 
-        I am a web developer based in Quebec, Canada. <br></br> 
-        Although I can do frontend work, I prefer working on the backend.
+        Hi, my name is Samuel Nguyen-Phok, and I’m a web developer based in Quebec, Canada. <br></br>
+        While I’m skilled in both frontend and backend development, my passion lies in backend work, where I can focus on building robust, 
+        scalable systems. <br></br>
+      </p>
+
+      <hr className='abouthr'></hr>
+
+      <p>
+        Beyond web development, I have a deep interest in programming languages—particularly their paradigms, 
+        such as functional programming—and enjoy exploring topics like compilers, interpreters, and even quirky esoteric languages. <br></br>
+      </p>
+
+      <hr className='abouthr'></hr>
+
+      <p>
+        In my free time, I create illustrations and comics, often for mature audiences. 
+        One of my proudest accomplishments is developing a personal comic publishing website, 
+        where I combined my technical skills with my creative passion.
+        <br></br>
+        <br></br>
+        I’m always excited to take on new challenges, whether it’s solving complex backend problems or diving into creative projects. 
       </p>
     </>
   );
