a5d5b4eaac9ccb7edc68ef34c7a7da7e5dccea66
WildSource <ilovetrap123@hotmail.com>
2025-01-25 15:30:35 -0500

    feat: add skills content

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -4,6 +4,27 @@ function Skills() {
   return (
     <>
       <h2>My Skill Sets</h2>
+      <h3>Languages</h3>
+      <ul>
+        <li>Java</li>
+        <li>Haskell</li>
+        <li>JavaScript</li>
+        <li>TypeScript</li>
+        <li>HTML</li>
+        <li>CSS</li>      
+      </ul>
+      <h3>Technologies</h3>
+      <ul>
+        <li>Spring Boot</li>
+        <li>Angular</li>
+        <li>React (Familiar)</li>
+        <li>Servant</li>
+      </ul>
+      <h3>Databases</h3>
+      <ul>
+        <li>MySQL</li>
+        <li>MariaDB</li>
+      </ul>
     </>
   );
 }
