129f8ed18353d017efd5e61b1a37f2c093e142f6
WildSource <ilovetrap123@hotmail.com>
2025-01-25 15:59:10 -0500

    feat: add content to Projects Section

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -2,15 +2,16 @@ import './Project.css';
 
 function Project() {
   return (
-    <>
+    <div className='projectContainer'>
       <h2><strong>My Projects</strong></h2>
-      <ul>
-        <li>
-          <h4>Waifuwebcomics.com</h4>
-          <iframe src='https://waifuwebcomics.com/' title='Waifuwebcomics'></iframe>
-        </li> 
-      </ul>
-    </>
+      <h4><a href='https://waifuwebcomics.com/'>Waifuwebcomics.com</a></h4>              
+      <iframe src='https://waifuwebcomics.com/' title='Waifuwebcomics'></iframe>
+      <p>
+        A webcomic publishing website written in TypeScript and Java. <br></br>
+        It utilize the Angular framework for the frontend and Spring Boot as it's backend. <br></br>
+        All of this is hosted on a DigitalOcean Droplet using MariaDB as its database.
+      </p>
+    </div>
   );
 }
 
