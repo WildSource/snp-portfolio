accc91341d6bab7c5fea7696f6820c4a0fb7ed81
dy/dx <ilovetrap123@hotmail.com>
2025-01-27 09:28:00 -0500

    feat: remove cover background to normal size and upscaled various fonts

--- src/components/title/Title.jsx
+++ src/components/title/Title.jsx
@@ -4,7 +4,7 @@ function Title() {
   return (
     <>
       <h1>Hello! I'm Samuel Nguyen-Phok.</h1>
-      <h4>I'm a web developer who prefers backend but can do fullstack.</h4>
+      <h3>I'm a web developer who prefers backend but can do fullstack.</h3>
     </>
   );
 }
