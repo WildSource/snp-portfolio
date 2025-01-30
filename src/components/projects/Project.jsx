41b0749b3431a8bc5ead08b21a8f0c72276a174f
dy/dx <ilovetrap123@hotmail.com>
2025-01-30 07:31:53 -0500

    feat: small mobile responsiveness implemented

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -2,16 +2,19 @@ import './Project.css';
 
 function Project() {
   return (
-    <div className='projectContainer'>
-            <h2><strong><a name='projects'></a>My Projects</strong></h2>
-      <h4><a href='https://waifuwebcomics.com/'>Waifuwebcomics.com</a></h4>              
-      <iframe src='https://waifuwebcomics.com/' title='Waifuwebcomics'></iframe>
-      <p>
+    <>
+      <h2><strong><a name='projects'></a>My Projects</strong></h2>
+      <div className='projectContainer'>
+        <h4><a href='https://waifuwebcomics.com/'>Waifuwebcomics.com</a></h4>              
+              <h5><strong>Warning Contains Nudity</strong></h5>
+        <iframe src='https://waifuwebcomics.com/' title='Waifuwebcomics'></iframe>
+        <p>
         A webcomic publishing website written in TypeScript and Java. <br></br>
         It utilize the Angular framework for the frontend and Spring Boot as it's backend. <br></br>
         All of this is hosted on a DigitalOcean Droplet using MariaDB as its database.
-      </p>
-    </div>
+        </p>
+      </div>
+    </>
   );
 }
 
