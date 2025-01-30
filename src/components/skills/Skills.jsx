3213810e966841d017eb80c7722dc9693eeed77c
dy/dx <ilovetrap123@hotmail.com>
2025-01-30 07:44:39 -0500

    adjust fonts on skills component

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -6,24 +6,24 @@ function Skills() {
       <h2><a name='skills'></a>My Skill Sets</h2>
       <h3>Languages</h3>
       <ul>
-        <li>Java</li>
-        <li>Haskell</li>
-        <li>JavaScript</li>
-        <li>TypeScript</li>
-        <li>HTML</li>
-        <li>CSS</li>      
+        <li><h2>Java</h2></li>
+        <li><h2>Haskell</h2></li>
+        <li><h2>JavaScript</h2></li>
+        <li><h2>TypeScript</h2></li>
+        <li><h2>HTML</h2></li>
+        <li><h2>CSS</h2></li>      
       </ul>
       <h3>Technologies</h3>
       <ul>
-        <li>Spring Boot</li>
-        <li>Angular</li>
-        <li>React (Familiar)</li>
-        <li>Servant</li>
+        <li><h2>Spring Boot</h2></li>
+        <li><h2>Angular</h2></li>
+        <li><h2>React (Familiar)</h2></li>
+        <li><h2>Servant</h2></li>
       </ul>
       <h3>Databases</h3>
       <ul>
-        <li>MySQL</li>
-        <li>MariaDB</li>
+        <li><h2>MySQL</h2></li>
+        <li><h2>MariaDB</h2></li>
       </ul>
     </>
   );
