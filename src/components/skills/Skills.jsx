48bbea4eb30e765cccf6accd2ad4ba99510a55b0
dy/dx <ilovetrap123@hotmail.com>
2025-02-06 07:23:28 -0500

    feat: add media query for tablets

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -5,26 +5,34 @@ function Skills() {
     <>
       <h2><a name='skills'></a>My Skill Sets</h2>
       <h3>Languages</h3>
-      <ul>
-        <li><h2>Java</h2></li>
-        <li><h2>Haskell</h2></li>
-        <li><h2>JavaScript</h2></li>
-        <li><h2>TypeScript</h2></li>
-        <li><h2>HTML</h2></li>
-        <li><h2>CSS</h2></li>      
-      </ul>
+      <div className='content'>
+        <ul>
+          <li><h2>Java</h2></li>
+          <li><h2>Haskell</h2></li>
+          <li><h2>JavaScript</h2></li>
+          <li><h2>TypeScript</h2></li>
+          <li><h2>HTML</h2></li>
+          <li><h2>CSS</h2></li>      
+        </ul>
+      </div>
+
       <h3>Technologies</h3>
-      <ul>
-        <li><h2>Spring Boot</h2></li>
-        <li><h2>Angular</h2></li>
-        <li><h2>React (Familiar)</h2></li>
-        <li><h2>Servant</h2></li>
-      </ul>
+      <div className='content'>
+        <ul>
+          <li><h2>Spring Boot</h2></li>
+          <li><h2>Angular</h2></li>
+          <li><h2>React (Familiar)</h2></li>
+          <li><h2>Servant</h2></li>
+        </ul>
+      </div>
+
       <h3>Databases</h3>
-      <ul>
-        <li><h2>MySQL</h2></li>
-        <li><h2>MariaDB</h2></li>
-      </ul>
+      <div className='content'>
+          <ul>
+            <li><h2>MySQL</h2></li>
+            <li><h2>MariaDB</h2></li>
+          </ul>
+      </div>
     </>
   );
 }
